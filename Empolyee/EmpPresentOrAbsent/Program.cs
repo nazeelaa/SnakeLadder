6714af1d9f1e9044108596a922e6c41e55cdc334
nazeelaa <nazeelakudar949@gmail.com>
2021-12-01 15:44:34 +0530

    Employee present or absent code is pushed

---
+++ Empolyee/EmpPresentOrAbsent/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EmployeePresentOrAbsent
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int isFullTime = 1;
+            Random random = new Random();//Random object has next method to gernerate random numbers
+            int empCheck = random.Next(0, 2);//generates 0 1 
+            if (empCheck == isFullTime)
+            {
+                Console.WriteLine("Employee is present");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
